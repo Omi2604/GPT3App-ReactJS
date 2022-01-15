d2b06edc50c5aff5295597feed553c2516a5a5b6
Omi2604 <omkarpimpude12@gmail.com>
2022-01-15 19:44:28 +0530

    Add files via upload

---
+++ src/components/cta/Cta.js
@@ -0,0 +1,17 @@
+import React from "react";
+import "./Cta.css";
+const Cta = () => {
+  return (
+    <div className="cta">
+      <div className="cta-content">
+        <p>Request Early access</p>
+        <h3>Register today & start exploring the possibilities!</h3>
+      </div>
+      <div className="cta-button">
+        <button type="button"> Get Started</button>
+      </div>
+    </div>
+  );
+};
+
+export default Cta;
