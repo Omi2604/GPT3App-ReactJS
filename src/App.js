85cabb7f4034178ca0eebe564ca3326a8c460deb
Omi2604 <omkarpimpude12@gmail.com>
2022-01-15 19:40:15 +0530

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,1 +1,33 @@
+import React from "react";
 
+import { Article, Brand, Cta, Feature, Navbar } from "./components";
+import {
+  Footer,
+  Blog,
+  Features,
+  Header,
+  Possibility,
+  WhatGPT3,
+} from "./containers";
+
+import "./App.css";
+
+const App = () => {
+  return (
+    <div className="App">
+      <div className="gradient__bg">
+        <Navbar />
+        <Header />
+      </div>
+      <Brand />
+      <WhatGPT3 />
+      <Features />
+      <Possibility />
+      <Cta />
+      <Blog />
+      <Footer />
+    </div>
+  );
+};
+
+export default App;
